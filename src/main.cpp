9d0479e71729f757b3c3bd608a33fd2b55bbc17d
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-02-03 01:21:03 +0300

    Initial Commit
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by valery on 03/02/2022.
+//
+
+#include "main.h"
+
+int main() {
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by valery on 03/02/2022.
+//
+
+#ifndef KALEIDOSCOPE_MAIN_H
+#define KALEIDOSCOPE_MAIN_H
+
+#endif //KALEIDOSCOPE_MAIN_H
